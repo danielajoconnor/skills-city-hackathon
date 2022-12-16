d58f8b16d68f3a6a4ffbf9e58594a44a946637e3
Daniel O'Connor <danielajoconnor@gmail.com>
2022-12-16 03:25:01 +0000

    added title animation

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// let title = document.getElementById("title-fade-default")
+// let titleOnHover = document.getElementById("title-fade-hover")
+
+// function titleHover() {
+//     title.onmouseover
+// }
