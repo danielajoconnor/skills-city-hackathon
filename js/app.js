524eb845cd4a8780b0c2383bac23a344a004d757
Daniel O'Connor <danielajoconnor@gmail.com>
2022-12-16 04:07:30 +0000

    folder fixes

--- js/app.js
+++
@@ -1,6 +0,0 @@
-// let title = document.getElementById("title-fade-default")
-// let titleOnHover = document.getElementById("title-fade-hover")
-
-// function titleHover() {
-//     title.onmouseover
-// }
